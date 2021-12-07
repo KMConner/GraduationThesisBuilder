a7ca49c954463859637e741c1e54f7a29395112b
KMConner <KMConner@users.noreply.github.com>
2021-12-07 17:28:16 +0900

    Install pandoc

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,14 @@ module Lib
     ( someFunc
     ) where
 
+import Text.Pandoc
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunccccc"
+someFunc = do
+    result <- runIO $ do
+        doc <- readMarkdown def (T.pack "[google](https://www.google.com/)")
+        writeRST def doc
+    rst <- handleError result
+    TIO.putStrLn rst
